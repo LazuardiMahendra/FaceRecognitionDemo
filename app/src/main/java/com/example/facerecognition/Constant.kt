0deafa6946cb5aa20b862d1377ef7df53a3604c1
LazuardiMahendra <lazuardhie.mahendra@gmail.com>
2025-07-23 01:23:41 +0800

    show key

--- app/src/main/java/com/example/facerecognition/Constant.kt
+++ app/src/main/java/com/example/facerecognition/Constant.kt
@@ -2,6 +2,10 @@ package com.example.facerecognition
 
 object Constant {
 
+//    const val API_KEY = ""
+//    const val ENDPOINT = ""
+
+
     const val DETECT_FACE = "face/v1.0/detect"
     const val VERIFY_FACE = "face/v1.0/verify"
 }
