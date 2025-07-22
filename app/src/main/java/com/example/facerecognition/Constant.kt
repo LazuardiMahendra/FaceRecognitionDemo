a7819fab46ff42e9db0d694b8275762f6745e6e6
LazuardiMahendra <lazuardhie.mahendra@gmail.com>
2025-07-23 01:22:29 +0800

    delete endpoint

--- app/src/main/java/com/example/facerecognition/Constant.kt
+++ app/src/main/java/com/example/facerecognition/Constant.kt
@@ -2,10 +2,6 @@ package com.example.facerecognition
 
 object Constant {
 
-//    const val API_KEY = ""
-//    const val ENDPOINT = ""
-
-
     const val DETECT_FACE = "face/v1.0/detect"
     const val VERIFY_FACE = "face/v1.0/verify"
 }
