1abc32c427d697d014c5b18dc45017dbc6ad8151
LazuardiMahendra <lazuardhie.mahendra@gmail.com>
2025-07-23 01:21:12 +0800

    resolved endpoint

--- app/src/main/java/com/example/facerecognition/Constant.kt
+++ app/src/main/java/com/example/facerecognition/Constant.kt
@@ -2,9 +2,8 @@ package com.example.facerecognition
 
 object Constant {
 
-    const val API_KEY =
-        ""
-    const val ENDPOINT = ""
+//    const val API_KEY = ""
+//    const val ENDPOINT = ""
 
 
     const val DETECT_FACE = "face/v1.0/detect"
